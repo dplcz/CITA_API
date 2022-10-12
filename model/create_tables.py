fcfcccd15d72ff693d9e176c3fe30581734b1d0c
dplcz <1061281835@qq.com>
2022-10-12 20:49:42 +0800

    创建所有表脚本

--- model/create_tables.py
+++ model/create_tables.py
@@ -4,10 +4,10 @@ from main_models import Base, AdminModel
 from config import config
 
 if __name__ == '__main__':
+    # 创建数据表
     engine = create_engine(
         'mysql+pymysql://{}:{}@{}:{}/{}'.format(config['mysqldb']['sql_user'], config['mysqldb']['sql_pass'],
                                                 config['mysqldb']['sql_host'], config['mysqldb']['sql_port'],
                                                 config['mysqldb']['database']), echo=True)
 
-    # AdminModel.metadata.create_all(engine)
     Base.metadata.create_all(engine)
