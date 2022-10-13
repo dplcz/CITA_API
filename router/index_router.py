e6ce67c85ee3cd73c295680331283a1c6d858a91
dplcz <1061281835@qq.com>
2022-10-13 19:07:24 +0800

    添加返回最新5个Acitivity接口

--- router/index_router.py
+++ router/index_router.py
@@ -9,14 +9,29 @@ from utils.async_util import db_session
 indexRouter = APIRouter()
 
 
+def get_dict_result(Model: object, fetch_res):
+    try:
+        filed_name = Model.__name__
+        result_temp = fetch_res.fetchall()
+        result = {'code': 0, 'data': []}
+        for i in result_temp:
+            temp = dict(i)[filed_name].__dict__
+            temp.pop('_sa_instance_state')
+            result['data'].append(temp)
+        return result
+    except Exception:
+        return {'code': 1}
+
+
 @indexRouter.get('/list-teacher')
 async def get_teacher_list(dbs: AsyncSession = Depends(db_session)):
     fetch_temp = await dbs.execute(select(TeacherModel))
-    filed_name = TeacherModel.__name__
-    result_temp = fetch_temp.fetchall()
-    result = {'code': 0, 'data': []}
-    for i in result_temp:
-        temp = dict(i)[filed_name].__dict__
-        temp.pop('_sa_instance_state')
-        result['data'].append(temp)
+    result = get_dict_result(TeacherModel, fetch_temp)
+    return result
+
+
+@indexRouter.get('/latest-activity')
+async def get_latest_activity(dbs: AsyncSession = Depends(db_session)):
+    fetch_temp = await dbs.execute(select(ActivityModel).limit(5).order_by(ActivityModel.time))
+    result = get_dict_result(ActivityModel, fetch_temp)
     return result
