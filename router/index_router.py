1838ef4ab62a6d28859c5198fc1797f6c1f1718c
dplcz <1061281835@qq.com>
2022-10-13 21:37:21 +0800

    修改select字段

--- router/index_router.py
+++ router/index_router.py
@@ -11,12 +11,11 @@ indexRouter = APIRouter()
 
 def get_dict_result(Model: object, fetch_res):
     try:
-        filed_name = Model.__name__
         result_temp = fetch_res.fetchall()
         result = {'code': 0, 'data': []}
         for i in result_temp:
-            temp = dict(i)[filed_name].__dict__
-            temp.pop('_sa_instance_state')
+            temp = dict(i)
+            # temp.pop('_sa_instance_state')
             result['data'].append(temp)
         return result
     except Exception:
@@ -25,13 +24,18 @@ def get_dict_result(Model: object, fetch_res):
 
 @indexRouter.get('/list-teacher')
 async def get_teacher_list(dbs: AsyncSession = Depends(db_session)):
-    fetch_temp = await dbs.execute(select(TeacherModel))
+    fetch_temp = await dbs.execute(
+        select(TeacherModel.id, TeacherModel.name, TeacherModel.img_url, TeacherModel.description,
+               TeacherModel.description))
     result = get_dict_result(TeacherModel, fetch_temp)
     return result
 
 
 @indexRouter.get('/latest-activity')
 async def get_latest_activity(dbs: AsyncSession = Depends(db_session)):
-    fetch_temp = await dbs.execute(select(ActivityModel).limit(5).order_by(ActivityModel.time))
+    fetch_temp = await dbs.execute(
+        select(ActivityModel.first_title, ActivityModel.second_title, ActivityModel.img_url, ActivityModel.time,
+               ActivityModel.detail_page_id, ActivityModel.detail_page_url).limit(3).order_by(
+            ActivityModel.time))
     result = get_dict_result(ActivityModel, fetch_temp)
     return result
