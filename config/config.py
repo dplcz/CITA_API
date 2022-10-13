59f03f0450484baf9575b2d2107bb63f6729a84a
dplcz <1061281835@qq.com>
2022-10-13 21:40:16 +0800

    不同系统配置文件

--- config/config.py
+++ config/config.py
@@ -1,11 +1,19 @@
 import configparser
+import platform
+
+plat = platform.system().lower()
+
+if plat == 'windows':
+    path = '\\'.join(__file__.split('\\')[:-1])
+    file_path = '{}\{}'.format(path, 'conf.ini')
+elif plat == 'linux':
+    path = '/'.join(__file__.split('/')[:-1])
+    file_path = '{}/{}'.format(path, 'conf.ini')
 
 # TODO 解析配置文件
 config = configparser.ConfigParser()
 
-path = '\\'.join(__file__.split('\\')[:-1])
-
-config.read_file(open('{}\{}'.format(path, 'conf.ini'), encoding='utf-8'))
+config.read_file(open(file_path, encoding='utf-8'))
 
 print('http binding on {}:{} '.format(config['web.conf']['ip_bind'], config['web.conf']['port_bind']))
 print('use sql by {}:{}'.format(config['mysqldb']['sql_host'], config['mysqldb']['sql_port']))
