0d88b27614631848cbcc42fae55fc5219507e898
dplcz <1061281835@qq.com>
2022-10-25 15:33:02 +0800

    添加新的测试活动

--- dao/insert_activity.py
+++ dao/insert_activity.py
@@ -27,11 +27,23 @@ if __name__ == '__main__':
     #                              img_url='https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/activity_3.jpg',
     #                              resize_img_url='https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/activity_3_resize.jpg',
     #                              detail_page_id=3, operation_time=datetime.datetime.now(), operation_user=1)
-    activity_xiang = ActivityModel(first_title='项目部第一次部门例会', second_title='欢迎来到科技创新协会',
-                                   time=datetime.datetime.strptime('2022-10-15', '%Y-%m-%d'),
-                                   img_url='https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/activity_4.jpg',
-                                   resize_img_url='https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/activity_4_resize.jpg',
-                                   detail_page_id=4, operation_time=datetime.datetime.now(), operation_user=1)
-    db_session.add_all([activity_xiang])
+    # activity_xiang = ActivityModel(first_title='项目部第一次部门例会', second_title='欢迎来到科技创新协会',
+    #                                time=datetime.datetime.strptime('2022-10-15', '%Y-%m-%d'),
+    #                                img_url='https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/activity_4.jpg',
+    #                                resize_img_url='https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/activity_4_resize.jpg',
+    #                                detail_page_id=4, operation_time=datetime.datetime.now(), operation_user=1)
+    activity_da = ActivityModel(first_title='科协全体大会', second_title='欢迎来到科技创新协会',
+                                time=datetime.datetime.strptime('2022-10-20', '%Y-%m-%d'),
+                                img_url='https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/activity_5.jpg',
+                                resize_img_url='https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/activity_5_resize.jpg',
+                                detail_page_id=5, detail_page_url='https://mp.weixin.qq.com/s/XDRj1wm_zAG8sstcx4cy7g',
+                                operation_time=datetime.datetime.now(), operation_user=1)
+    activity_po = ActivityModel(first_title='破冰晚会', second_title='欢迎来到科技创新协会',
+                                time=datetime.datetime.strptime('2022-10-21', '%Y-%m-%d'),
+                                img_url='https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/activity_6.jpg',
+                                resize_img_url='https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/activity_6_resize.jpg',
+                                detail_page_id=6, detail_page_url='https://mp.weixin.qq.com/s/o4o3f8qXA2-tvtxhC-VumQ',
+                                operation_time=datetime.datetime.now(), operation_user=1)
+    db_session.add_all([activity_da, activity_po])
     db_session.commit()
     db_session.close()
