2e18e18ad8761095954195457146e9e6e449fbda
dplcz <1061281835@qq.com>
2022-10-28 10:53:54 +0800

    添加登录路由

--- main_app.py
+++ main_app.py
@@ -5,12 +5,14 @@ from starlette.middleware.cors import CORSMiddleware
 from config.config import config
 
 from router.index_router import indexRouter
+from router.manage_router import manageRouter
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=False, allow_methods=["*"],
                    allow_headers=["*"], )
 
 app.include_router(indexRouter, prefix='/index', tags=['主页信息路由'])
+app.include_router(manageRouter, prefix='/man', tags=['管理页面路由'])
 
 if __name__ == '__main__':
     uvicorn.run(app, host="{}".format(config['web.conf'].get('ip_bind', '127.0.0.1')),
