ca323288e5ae41f6d7abbe8b574f605f6c4f3e6d
dplcz <1061281835@qq.com>
2022-10-28 10:55:09 +0800

    校验和生成token工具

---
+++ utils/token_util.py
@@ -0,0 +1,26 @@
+import jwt
+from jwt.exceptions import ExpiredSignatureError, PyJWTError
+from datetime import datetime, timedelta
+from config.config import config
+
+SECRET_KEY = config['web.conf']['SECRET_KEY']
+
+
+def judge_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms='HS256')
+        # print(payload)
+        return payload['user']
+    except ExpiredSignatureError:
+        # print('token过期了')
+        return None
+    except PyJWTError:
+        # print('token验证失败')
+        return None
+
+
+def create_token(username):
+    expires = datetime.utcnow() + timedelta(hours=1)
+    to_encode = {'exp': expires, 'user': username}
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm='HS256')
+    return encode_jwt
