2284ae8c27565547cb3f14ac143a1fc624e16319
dplcz <1061281835@qq.com>
2022-10-31 22:03:34 +0800

    修改跨域中间件

--- main_app.py
+++ main_app.py
@@ -8,7 +8,8 @@ from router.index_router import indexRouter
 from router.manage_router import manageRouter
 
 app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"],
+app.add_middleware(CORSMiddleware, allow_origins=["http://124.221.228.222:9800", "http://192.168.21.1:9800"],
+                   allow_credentials=True, allow_methods=["*"],
                    allow_headers=["*"], )
 
 app.include_router(indexRouter, prefix='/index', tags=['主页信息路由'])
