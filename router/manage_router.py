0de6de5aff5d97b9bb26ae051e492a6e4b05868e
dplcz <1061281835@qq.com>
2022-11-03 00:42:02 +0800

    添加插入数据接口

--- router/manage_router.py
+++ router/manage_router.py
@@ -153,7 +153,7 @@ async def insert(insert_type: ModelType, token: str = Cookie(...), data: dict = 
             temp_model = data_model(**data)
             dbs.add(temp_model)
             await dbs.commit()
-            return Response(status_code=201, content={'code': 0}, )
+            return {'code': 0}
         except ValueError:
             return Response(status_code=404, content='字段错误！')
     else:
