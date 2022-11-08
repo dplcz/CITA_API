f767d202d16018b6b60e2f0cc6d874f4746a1931
dplcz <1061281835@qq.com>
2022-11-08 09:11:56 +0800

    修改提取配置格式

--- model/create_tables.py
+++ model/create_tables.py
@@ -5,8 +5,8 @@ from config.config import config
 if __name__ == '__main__':
     # 创建数据表
     engine = create_engine(
-        'mysql+pymysql://{}:{}@{}:{}/{}'.format(config['mysqldb']['sql_user'], config['mysqldb']['sql_pass'],
-                                                config['mysqldb']['sql_host'], config['mysqldb']['sql_port'],
-                                                config['mysqldb']['database']), echo=True)
+        'mysql+pymysql://{}:{}@{}:{}/{}'.format(config['conf']['mysqldb']['user'], config['conf']['mysqldb']['pass'],
+                                                config['conf']['mysqldb']['host'], config['conf']['mysqldb']['port'],
+                                                config['conf']['mysqldb']['database']), echo=True)
 
     Base.metadata.create_all(engine)
