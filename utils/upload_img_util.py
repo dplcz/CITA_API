13f72797308936690038847ea42f0a0a247f2dae
dplcz <1061281835@qq.com>
2022-11-08 09:14:29 +0800

    添加图片上传类，添加lsky图库上传功能

--- utils/upload_img_util.py
+++ utils/upload_img_util.py
@@ -3,29 +3,45 @@ from qcloud_cos import CosConfig
 from qcloud_cos import CosS3Client
 from config.config import config
 from PIL import Image
-
-cos_config = CosConfig(Region=config['upload.conf']['region'], SecretId=config['upload.conf']['secret_id'],
-                       SecretKey=config['upload.conf']['secret_key'], Token=config['upload.conf']['token'])
-client = CosS3Client(cos_config)
+import httpx
 
 
-def resize_img(img_content: bytes, width: int, height: int, filename, file_type) -> bool:
-    img = Image.open(BytesIO(img_content))
-    out = img.resize((width, height), Image.ANTIALIAS)
-    ouy_bytes = BytesIO()
-    out.save(ouy_bytes, format=file_type)
+class ImageUploader:
+    def __init__(self):
+        self.type = config['conf']['upload']['type']
+        if self.type == 'cos':
+            cos_config = CosConfig(Region=config['conf']['upload']['cos']['region'],
+                                   SecretId=config['conf']['upload']['cos']['secret_id'],
+                                   SecretKey=config['conf']['upload']['cos']['secret_key'],
+                                   Token=config['conf']['upload']['cos']['token'])
+            self.client = CosS3Client(cos_config)
 
-    return upload_file(ouy_bytes.getvalue(), filename)
+    def resize_img(self, img_content: bytes, width: int, height: int, filename, file_type):
+        img = Image.open(BytesIO(img_content))
+        out = img.resize((width, height), Image.ANTIALIAS)
+        ouy_bytes = BytesIO()
+        out.save(ouy_bytes, format=file_type)
 
+        return self.upload_file(ouy_bytes.getvalue(), filename)
 
-def upload_file(file_content: bytes, filename) -> bool:
-    try:
-        response = client.put_object(
-            Bucket=config['upload.conf']['bucket'],
-            Body=file_content,
-            Key=filename,
-            EnableMD5=False
-        )
-        return True
-    except Exception:
-        return False
+    def upload_file(self, file_content: bytes, filename):
+        if self.type == 'cos':
+            try:
+                response = self.client.put_object(
+                    Bucket=config['conf']['upload'][self.type]['bucket'],
+                    Body=file_content,
+                    Key=filename,
+                    EnableMD5=False
+                )
+                return True
+            except Exception:
+                return False
+        elif self.type == 'lsky':
+            try:
+                headers = {'Accept': 'application/json',
+                           'Authorization': config['conf']['upload'][self.type]['authorization']}
+                response = httpx.post(config['conf']['upload'][self.type]['host'], headers=headers,
+                                      files={'file': (filename, file_content)}).json()
+                return response['data']['links']['url']
+            except Exception:
+                return None
